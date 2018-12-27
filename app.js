8c66fa60d89567b066c921bb0894805526e19754
Brian Michael Romano <brianmichaelromano@gmail.com>
2018-12-27 01:07:29 +0000

    Update

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const HOST = 'localhost';
 const PORT = process.env.PORT || 5000;
 
 app.use('/', (req, res, next) => {
   res.send('Hello Express!');
 });
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, HOST, () => console.log(`Server listening at ${HOST} on port ${PORT}`));
