3d3c81f0ceb8c98a992cf5502bebff8a650b17ba
Brian <brianmromano@gmail.com>
2018-12-28 17:51:36 -0500

    now uses ejs templating engine and has views and for index and welcome

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const app = express();
 const HOST = 'localhost';
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.get('/welcome', (req, res, next) => {
-  res.send('Welcome');
+  res.render('welcome');
 });
 
 app.get('/', (req, res, next) => {
-  res.send('Hello Express updated!');
+  res.render('index');
 });
 
 app.listen(PORT, HOST, () => console.log(`Server listening at ${HOST} on port ${PORT}`));
