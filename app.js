8600d001586ceb41f88b83bdbfc2b304e15724e8
Brian <brianmromano@gmail.com>
2018-12-29 04:52:08 -0500

    update

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-const rootDir = require('./util/rootDir.js');
-
-const HOST = 'localhost';
 const PORT = process.env.PORT || 5000;
 
-app.set('views', 'views');
-app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(rootDir, 'public')));
-
-app.get('/welcome', (req, res, next) => {
-  res.render('welcome');
-});
+app.use(express.static('public'));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.send('index.html');
 });
 
-app.listen(PORT, HOST, () => console.log(`Server listening at ${HOST} on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
